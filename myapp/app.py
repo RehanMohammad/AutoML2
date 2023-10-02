3a6532be955ad29cec0ac6ebd3e2d06d8ae2b98f
mohammad rehan <mohammadrehan934@gmail.com>
2023-10-02 12:27:46 +0530

    login/register
    add login/ register pages and function and create a data base for Users

--- myapp/app.py
+++ myapp/app.py
@@ -1,22 +1,40 @@
+import datetime
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_bcrypt import Bcrypt
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SubmitField, FileField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SelectField, SubmitField, FileField, PasswordField
+from wtforms.validators import DataRequired, InputRequired, Length, ValidationError
 import pandas as pd
 from supervised.automl import AutoML
 import os
 import shutil
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecretkey'
-app.config['UPLOAD_FOLDER'] = './uploads'
+application = Flask(__name__)
+application.config['SECRET_KEY'] = 'mysecretkey'
+application.config['UPLOAD_FOLDER'] = './uploads'
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+application.config['SECRET_KEY'] = 'thisisasecretkey'
+db = SQLAlchemy(application)
+bcrypt = Bcrypt(application)
+
+
+login_manager = LoginManager()
+login_manager.init_application(application)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class DataUploadForm(FlaskForm):
     file = FileField('Upload CSV with training data', validators=[DataRequired()])
     submit = SubmitField('Upload and Proceed')
-
-
 class ModelTrainingForm(FlaskForm):
     x_columns = StringField('Input features (comma-separated)', validators=[DataRequired()])
     y_column = StringField('Target column', validators=[DataRequired()])
@@ -26,22 +44,58 @@ class ModelTrainingForm(FlaskForm):
                              choices=[('60', '60'), ('120', '120'), ('240', '240'), ('300', '300')])
     submit = SubmitField('Start Training')
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    # username = db.Column(db.String(20), nullable=False, unique=True)
+    email= db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    data_added= db.column(db.DateTime)
 
-@app.route('/', methods=['GET', 'POST'])
+
+class RegisterForm(FlaskForm):
+    email= StringField(validators=[
+                           InputRequired(), Length(min=12, max=100)], render_kw={"placeholder": "email"})
+    
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Register')
+    def validate_email(self, email):
+        existing_email = User.query.filter_by(
+            email=email.data).first()
+        if existing_email:
+            raise ValidationError(
+                'That email already exists. Please choose a different one.')
+        
+class LoginForm(FlaskForm):
+    email = StringField(validators=[
+                           InputRequired(), Length(min=12, max=120)], render_kw={"placeholder": "email"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
+
+
+
+
+
+@application.route('/', methods=['GET', 'POST'])
 def home():
     form = DataUploadForm()
     if form.validate_on_submit():
-        if not os.path.exists(app.config['UPLOAD_FOLDER']):
-            os.makedirs(app.config['UPLOAD_FOLDER'])
+        if not os.path.exists(application.config['UPLOAD_FOLDER']):
+            os.makedirs(application.config['UPLOAD_FOLDER'])
 
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], form.file.data.filename)
+        filepath = os.path.join(application.config['UPLOAD_FOLDER'], form.file.data.filename)
         form.file.data.save(filepath)
 
         return redirect(url_for('train_model', filepath=filepath))
     return render_template('home.html', form=form)
 
 
-@app.route('/train', methods=['GET', 'POST'])
+@application.route('/train', methods=['GET', 'POST'])
 def train_model():
     filepath = request.args.get('filepath')
     form = ModelTrainingForm()
@@ -58,5 +112,46 @@ def train_model():
     return render_template('train.html', form=form)
 
 
+@application.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
+
+
+@application.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@application.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@ application.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug= True)
